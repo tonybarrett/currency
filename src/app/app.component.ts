32869d14a04124a5d9e20972684bad69e0a23463
Tony Barrett <tonyb@gmx.com>
2025-03-22 13:57:17 +0000

    Initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertionComponent } from "./component/convertion/convertion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ConvertionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/component/convertion/convertion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertion',
+  standalone: true,
+  imports: [],
+  templateUrl: './convertion.component.html',
+  styleUrl: './convertion.component.scss'
+})
+export class ConvertionComponent {
+
+}
